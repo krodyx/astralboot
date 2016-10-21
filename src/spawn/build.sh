2cbcd65537b3cba8200f97550938540f1f582e8d
Simon Kirkby <tigger@interthingy.com>
2016-10-20 23:09:53 -0400

    updated build spawn script

--- src/spawn/build.sh
+++ src/spawn/build.sh
@@ -2,14 +2,16 @@
 echo Building static spawn
 CGO_ENABLED=0 go build -ldflags '-extldflags "-static"'
 echo Building RKT ACI
+tag=`git describe --tags 2>/dev/null`
 acbuild begin
 acbuild set-name astralboot/rocket/spawn
 acbuild copy spawn /bin/spawn
 acbuild set-exec /bin/spawn
-acbuild label add version 0.0.1
+acbuild label add version $tag
 acbuild label add arch amd64
 acbuild label add os linux
 acbuild annotation add authors "Simon Kirkby tigger@interhingy.com"
-acbuild write spawn-0.0.1-linux-amd64.aci
+acbuild write --overwrite spawn-$tag-linux-amd64.aci
 acbuild end
+gpg --sign --detach-sig -a spawn-$tag-linux-amd64.aci
 echo Finished
